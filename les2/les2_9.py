e0a3fd646689079d0eab528cb51e238130218f30
Randy-jun <275212517@qq.com>
2015-10-10 20:29:39 +0800

    add dict.duple,list,set,etc.

---
+++ les2/les2_9.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+s1 = set([1, 2, 3, 1])
+s = set([1, 2, 3, 1])
+print(s)
+print(s1)
+s.add(4)
+print(s)
+s.remove(3)
+print(s)
+s2 = set([1, 2, 4])
+s3 = set([1, 2, 3])
+print(s2 & s3)
+print(s2 | s3)
+a = ['d', 'c', 'a', 'b']
+a.sort()
+print(a)
+aa = 'abcbs'
+print(aa.replace('b', 'a'))
+print(aa)
