363a135c523ca369caa40a43b0710c23a0a60f74
Randy-jun <275212517@qq.com>
2015-10-10 15:34:33 +0800

    Second Day list and tuple

--- les2/les2_2.py
+++ les2/les2_2.py
@@ -1,3 +1,22 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
+# list
+classmates = ['Michael', 'Bob', 'Tracy']
+print(classmates)
+print(len(classmates))
+print(classmates[-1])
+print(classmates[0])
+classmates.append('Adam')
+print(classmates)
+print(classmates.pop(1))
+print(classmates)
+classmates[1] = 'Sarah'
+print(classmates)
+L = ['Apple', 123, True]
+print(L)
+s = ['python', 'java', ['asp', 'php'], 'scheme']
+print(s)
+print(len(s))
+print(s[2][1])
+l = []
+print(len(l))
---
+++ les2/les2_3.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#tuple can not be redefine               
+classmate_t = ('Machael', 'Bob', 'Tracy')
+print(classmate_t)                    
