9219779fb6f74937a66f2a4650ba0daee8a42337
Randy-jun <275212517@qq.com>
2015-10-14 08:47:06 +0800

    The forth day end

--- les3/les3_6.py
+++ les3/les3_6.py
@@ -9,3 +9,34 @@ def fact(n):
 print(fact(1))
 print(fact(5))
 print(fact(15))
+print(fact(998))
+
+#栈溢出解决方案，尾递归（和循环效果一样）
+def facta(n):
+    return facta_iter(n, 1)
+
+def facta_iter(num, product):
+    if num == 1:
+        return product
+    return facta_iter(num - 1, num * product)
+
+print(facta(1))
+print(facta(5))
+print(facta(15))
+print(facta(997))
+#print(facta(1000))
+#汉诺塔问题
+
+import time
+
+def move(n, a, b, c):
+    if n == 1:
+        print('move', a, '---->', c)
+        return
+    move(n - 1, a, c, b)
+    print('move', a, '---->', c)
+    move(n - 1, b, a, c)
+t_start = time.clock()
+move(50, 'A', 'B', 'C')
+t_end = time.clock()
+print(t_end - t_start)
