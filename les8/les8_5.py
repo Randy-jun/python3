2fd3500629176b58a612ee769fd8c4d5750d2004
Randy-jun <275212517@qq.com>
2016-03-14 21:19:36 +0800

    开始枚举类

---
+++ les8/les8_5.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# 使用枚举类
+
+from enum import Enum
+
+Month = Enum('Month', ('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',\
+    'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'))
+
+for name, member in Month.__members__.items():
+    print(name, '=>', member, ',', member.value)
+#value属性则是自动赋给成员的int常量，默认从1开始计数。
+
