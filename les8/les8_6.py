cbc1c6ed449cc68205d21baabbd4faa2fe00e9a5
Randy-jun <275212517@qq.com>
2016-03-22 22:06:04 +0800

    keep doing

--- les8/les8_6.py
+++ les8/les8_6.py
@@ -32,7 +32,8 @@ print('------------------------------------------------')
 3.class的方法名称与函数绑定，这里我们把函数fn绑定到方法名hello1上。
 
 通过type()函数创建的类和直接写class是完全一样的，因为Python解释器遇
-到class定义时，仅仅是扫描一下class定义的语法，然后调用type()函数创建出class。
+到class定义时，仅仅是扫描一下class定义的
+语法，然后调用type()函数创建出class。
 
 metaclass
 
@@ -42,7 +43,48 @@ metaclass，直译为元类，简单的解释就是：
 但是如果我们想创建出类呢？那就必须根据metaclass创建出类，所以：先定义metaclass，然后创建类。
 连接起来就是：先定义metaclass，就可以创建类，最后创建实例。
 所以，metaclass允许你创建类或者修改类。换句话说，你可以把类看成是metaclass创建出来的“实例”。
-metaclass是Python面向对象里最难理解，也是最难使用的魔术代码。正常情况下，你不会碰到需要
-使用metaclass的情况，所以，以下内容看不懂也没关系，因为基本上你不会用到。
+metaclass是Python面向对象里最难理解，也是最难使用的魔术代码。正常情况下，你不会碰到需要使用
+metaclass的情况，所以，以下内容看不懂也没关系，因为基本上你不会用到。
 '''
+#metaclass是类的模板，所以必须从'type'类派生
+class ListMetaclass(type):
+    def __new__(cls, name, bases, attrs):
+        attrs['add'] = lambda self, value: self.append(value)
+        return type.__new__(cls, name, bases, attrs)
 
+class MyList(list, metaclass=ListMetaclass):
+    pass
+
+L = MyList()
+L.add(1)
+print(L)
+
+L2 = list()
+#L2.add(1)
+
+class Field(object):
+
+    def __init__(self, name, column_type):
+        self.name = name
+        self.column_type = column_type
+
+    def __str__(self):
+        return '<%s:%s>' % (self.__class__.__name__, self.name)
+class StringField(Field):
+    
+    def __inti__(self, name):
+        super(StringField, self).__init__(name, 'varchar(100)')
+
+class IntegerField(Field):
+    
+    def __inti__(self, name):
+        super(IntegerField, self).__init__(name, 'varchar(100)')
+
+class User(Model):
+    id = IntegerField('id')
+    name = StringField('username')
+    email = StringField('email')
+    password = StringField('password')
+
+u = User(id=12345, name='Michael', email='test@test.com', password='my-pwd')
+u.save()
