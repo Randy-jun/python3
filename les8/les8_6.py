c06c66fb1091a2c8c5a77ea97ca5a25802adaa2c
Randy-jun <275212517@qq.com>
2016-03-22 22:35:14 +0800

    almost finish it

--- les8/les8_6.py
+++ les8/les8_6.py
@@ -6,7 +6,8 @@ class Hello(object):
     def hello(self, name='world'):
         print('Hello, %s.' % name)
 
-from les8_6 import Hello
+# 下面这行注释要看情况去掉
+#from les8_6 import Hello
 
 h = Hello()
 h.hello()
@@ -70,15 +71,60 @@ class Field(object):
 
     def __str__(self):
         return '<%s:%s>' % (self.__class__.__name__, self.name)
+
 class StringField(Field):
     
-    def __inti__(self, name):
+    def __init__(self, name):
         super(StringField, self).__init__(name, 'varchar(100)')
 
 class IntegerField(Field):
     
-    def __inti__(self, name):
-        super(IntegerField, self).__init__(name, 'varchar(100)')
+    def __init__(self, name):
+        super(IntegerField, self).__init__(name, 'bigint')
+
+class ModelMetaclass(type):
+
+    def __new__(cls, name, bases, attrs):
+        if name=='Model':
+            return type.__new__(cls, name, bases, attrs)
+        print('Found model: %s' % name)
+        mappings = dict()
+        for k, v in attrs.items():
+            if isinstance(v, Field):
+                print('Found mapping: %s ==> %s' % (k, v))
+                mappings[k] = v
+        for k in mappings.keys():
+            attrs.pop(k)
+        attrs['__mappings__'] = mappings # 保存属性和列的映射关系
+        attrs['__table__'] = name # 假设表明和类名一致
+        return type.__new__(cls, name, bases, attrs)
+
+class Model(dict, metaclass = ModelMetaclass):
+
+    def __init__(self, **kw):
+        super(Model, self).__init__(**kw)
+
+    def __getattr__(self, key):
+        try:
+            return self[key]
+        except KeyError:
+            raise AttributeError(r"'Model' object ha no attribute '%s'." % key)
+    
+    def __setattr__(self, key, value):
+        self[key] = value
+
+    def save(self):
+        fields = []
+        params = []
+        args = []
+        for k, v in self.__mappings__.items():
+            fields.append(v.name)
+            params.append('?')
+            args.append(getattr(self, k, None))
+        sql = 'insert into %s (%s) values (%s)' % (self.__table__, ','.join(fields), ','.join(params))
+        print('SQL: %s' % sql)
+        print('ARGS: %s' % str(args))
+
 
 class User(Model):
     id = IntegerField('id')
