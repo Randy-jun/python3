0eb8ec68406ca78c807ae80c3c01026cdd12258a
Randy-jun <275212517@qq.com>
2017-03-20 17:18:57 +0800

    finish metaclass

--- les9/les9_1.py
+++ les9/les9_1.py
@@ -3,7 +3,144 @@
 
 # 使用元类
 
-class Hello(object):
-    def hello(self, name='world'):
-        print('Hello, %s.' % name)
+#class Hello(object):
+#    def hello(self, name='world'):
+#        print('Hello, %s.' % name)
+#
+#h = Hello()
+#h.hello()
+#
+#print(type(Hello))
+#print(type(h))
+#
+''' type()函数创建类'''
+def fn1(self, name='world'): # 先定义函数
+    print('Hello, %s.' % name)
 
+Hello = type('Hello', (object,), dict(hello=fn1)) # 创建Hello类
+
+h = Hello()
+h.hello()
+print(type(Hello))
+print(type(h))
+'''
+要创建一个class对象，type()函数依次传入3个参数：
+
+1. class的名称；
+2. 继承的父类集合，注意Python支持多重继承，如果只有一个父类，别忘了tuple的单元素写法；
+3. class的方法名称与函数绑定，这里我们把函数fn绑定到方法名hello上。
+
+通过type()函数创建的类和直接写class是完全一样的，因为Python解释器遇到class定义时，仅仅是扫描一下class定义
+的语法，然后调用type()函数创建出class。
+
+正常情况下，我们都用class Xxx...来定义类，但是，type()函数也允许我们动态创建出类来，也就是说，动态语言本身
+支持运行期动态创建类，这和静态语言有非常大的不同，要在静态语言运行期创建类，必须构造源代码字符串再调用编译
+器，或者借助一些工具生成字节码实现，本质上都是动态编译，会非常复杂。
+'''
+'''
+metaclass 元类
+
+当我们定义了类以后，就可以根据这个类创建出实例，所以：先定义类，然后创建实例。
+但是如果我们想创建出类呢？那就必须根据metaclass创建出类，所以：先定义metaclass，然后创建类。
+连接起来就是：先定义metaclass，就可以创建类，最后创建实例。
+所以，metaclass允许你创建类或者修改类。换句话说，你可以把类看成是metaclass创建出来的“实例”。
+metaclass是Python面向对象里最难理解，也是最难使用的魔术代码。正常情况下，你不会碰到需要使用metaclass的情况，
+所以，以下内容看不懂也没关系，因为基本上你不会用到。
+'''
+# metaclass是类的模板，所以必须从'type'类型派生
+class ListMetaclass(type):
+    def __new__(cls, name, bases, attrs):
+        attrs['add'] = lambda self, value: self.append(value)
+        return type.__new__(cls, name, bases, attrs)
+
+class MyList(list, metaclass = ListMetaclass):
+    pass
+'''
+__new__()方法接收到的参数依次是：
+1. 当前准备创建的类的对象；
+2. 类的名字；
+3. 类继承的父类集合；
+4. 类的方法集合。
+'''
+L = MyList()
+L.add(0)
+print(L)
+
+
+'''
+普通的list没有add()方法
+L1 = list()
+L1.add(1)
+print(L1)
+'''
+
+class Field(object):
+
+    def __init__(self, name, column_type):
+        self.name = name
+        self.column_type = column_type
+
+    def __str__(self):
+        return '<%s:%s>' % (self.__class__.__name__, self.name)
+
+class StringField(Field):
+
+    def __init__(self, name):
+        super(StringField, self).__init__(name, 'varchar(100)')
+
+class IntegerField(Field):
+
+    def __init__(self, name):
+        super(IntegerField, self).__init__(name, 'bigint')
+
+class ModelMetaclass(type):
+
+    def __new__(cls, name, bases, attrs):
+        if name == 'Model':
+            return type.__new__(cls, name, bases, attrs)
+        print('Found mode: %s' % name)
+        mappings = dict()
+        for k, v in attrs.items():
+            if isinstance(v, Field):
+                print('Found mapping: %s ==> %s' % (k, v))
+                mappings[k] = v
+        for k in mappings.keys():
+            attrs.pop(k)
+        attrs['__mappings__'] = mappings
+        attrs['__table__'] = name
+        return type.__new__(cls, name, bases, attrs)
+
+class Model(dict, metaclass = ModelMetaclass):
+
+    def __init__(self, **kw):
+        super(Model, self).__init__(**kw)
+
+    def __getattr__(self, key):
+        try:
+            return self[key]
+        except KeyError:
+            raise AttributeError(r"'Model' object has no attribute '%s'" % key)
+    
+    def __setattr__(self, key, value):
+        self[key] = value
+
+    def save(self):
+        fields  = []
+        params  = []
+        args    = []
+        for k, v in self.__mappings__.items():
+            fields.append(v.name)
+            params.append('?')
+            args.append(getattr(self, k, None))
+        sql = 'insert into %s (%s) values (%s)' % (self.__table__, ','.join(fields), ','.join(params))
+        print('SQL: %s' % sql)
+        print('ARGS: %s' % str(args))
+
+class User(Model):
+    id       = IntegerField('id')
+    name     = StringField('username')
+    email    = StringField('umail')
+    password = StringField('password')
+
+u = User(id = 12345, name = 'Randy', email = 'Randy@test.com', password = 'my-pwd')
+u.save()
